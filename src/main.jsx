ae999aad04243c0dff5eb07bc8506c2271b6309a
Soban Saeed <sobansaeed123@gmail.com>
2025-06-02 16:52:06 +0100

    reverted

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App2 from './App2.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App2 />
+    <App />
 )
