f17a96e51c9b796166614fb93041e2e54db6acd5
Soban Saeed <sobansaeed123@gmail.com>
2025-06-04 17:02:21 +0100

    another responsive design

--- src/components/homepage/testimonial-components/TestimonialCard.jsx
+++ src/components/homepage/testimonial-components/TestimonialCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TestimonialCard = ({ testimonialText, name, location, image }) => {
   return (
-    <div className="bg-white w-[400px] sm:w-[550px] h-[300px] rounded-2xl flex flex-col p-10 gap-10">
+    <div className="bg-white w-[350px] sm:w-[550px] h-[300px] rounded-2xl flex flex-col p-10 gap-10">
       <div className="flex items-start justify-between">
         <div className="w-fit h-fit text-left flex gap-5">
           <img
