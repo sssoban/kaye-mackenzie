9bfe6dad63b8cc140a095508705be39c119ceaf9
Soban Saeed <sobansaeed123@gmail.com>
2025-06-05 16:06:35 +0100

    text changes

--- src/components/homepage/FrontSection.jsx
+++ src/components/homepage/FrontSection.jsx
@@ -1,10 +1,17 @@
-
 const FrontSection = () => {
   return (
     <div className="w-full px-4 h-[720px] bg-[#204396] text-white flex flex-col items-center justify-center gap-4 sm:gap-0">
-      <img src='\KayeMackenzieLogoWithBG.png' alt='logo' className="sm:hidden w-[80%] "/>
-      <p className="font-bold italic sm:text-[70px] text-[40px]">
-        Real Estate Expertise. IT Solutions Excellence.
+      <img
+        src="\KayeMackenzieLogoWithBG.png"
+        alt="logo"
+        className="sm:hidden w-[80%] "
+      />
+      <p className="sm:hidden font-bold italic sm:text-[70px] text-[30px]">
+        • Real Estate Expertise <br className="sm:hidden"/> • IT Solutions Excellence
+      </p>
+      <p className="hidden sm:flex font-bold italic text-[70px]">
+        Real Estate Expertise. IT Solutions.
+        Excellence
       </p>
       <p className="italic text-2xl sm:text-[25px] font-lato">
         From property investments to cutting-edge digital solutions, we bring
--- src/components/itpage/SuccessSection.jsx
+++ src/components/itpage/SuccessSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const stats = [
-  { value: "9", label: "Happy Clients" },
-  { value: "14", label: "Completed Projects" },
+  { value: "Many", label: "Happy Customers" },
+  { value: "Numerous", label: "Completed Projects" },
   { value: "10K+", label: "Transactions" },
   { value: "6000+", label: "Customers" },
 ];
@@ -12,7 +12,7 @@ const SuccessSection = () => {
     <div className="sm:h-[500px] bg-[#204396] w-full flex flex-col sm:flex-row items-center justify-center text-white gap-20 py-10 sm:py-0">
       {stats.map((stat) => (
         <div key={stat.label} className="flex flex-col items-center gap-5">
-          <p className="text-8xl font-bold">{stat.value}</p>
+          <p className="text-5xl sm:text-7xl font-bold">{stat.value}</p>
           <p className="text-2xl">{stat.label}</p>
         </div>
       ))}
