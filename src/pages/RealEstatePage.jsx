501f547d19aff57a440f7dbf6bbe9aba58246e05
Soban Saeed <sobansaeed123@gmail.com>
2025-06-05 15:24:30 +0100

    text changes

--- src/pages/RealEstatePage.jsx
+++ src/pages/RealEstatePage.jsx
@@ -55,11 +55,11 @@ function RealEstatePage() {
     <div className="background-image">
       <img src="KayeMackenzieLogo.png" alt="Kaye Mackenzie Logo" className='pt-12 h-50 sm:h-70 mx-auto'/>
       <div className='sm:mt-20 mx-2'>
-        <h1 className='text-[35px] sm:text-[65px] text-[#1E3C87] font-bold'>Something Exciting Is Coming</h1>
+        <h1 className='text-[35px] sm:text-[65px] text-[#1E3C87] font-bold'>Page Update in Progress</h1>
         <p className=' sm:text-[20px] text-[#4B5563]'>We're working on something special and can't wait to share it with you.</p>
       </div>
       <div className='mt-10 mb-4'>
-        <p className='font-poppins-regular text-[20px] text-[#4B5563]'>Be the first to know when we launch!</p>
+        <p className='font-poppins-regular text-[20px] text-[#4B5563]'>Be the first to know when it goes live!</p>
       </div>
       <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
         <input 
